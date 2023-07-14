6c4664b5c230f2fe56697e3e7f09a4bce09b7edb
Alex Vinamagua <alex.vinamagua@unl.edu.ec>
2023-07-14 16:28:39 -0500

    "definicion de los primeros modelos del proyecto"

--- sistema_tutorias/admin.py
+++ sistema_tutorias/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Persona, Carrera, Asignatura
 
-# Register your models here.
+@admin.register(Persona)
+class PersonaAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Carrera)
+class CarreraAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Asignatura)
+class Asignatura(admin.ModelAdmin):
+    pass
--- sistema_tutorias/models.py
+++ sistema_tutorias/models.py
@@ -1,3 +1,45 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+
+class Persona(models.Model):
+    OPCIONES_CARGO = (
+        ('decano', 'Decano'),
+        ('docente', 'Docente'),
+        ('estudiante', 'Estudiante'),
+        ('director', 'Director'),
+    )
+
+    cedula = models.CharField(max_length=20)
+    nombres = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    cargo = models.CharField(max_length=20, choices=OPCIONES_CARGO)
+
+    def info_docente(self):
+        return "|{}|{}|  {} {}".format(self.cedula, self.cargo, self.nombres, self.apellidos)
+
+    def __str__(self):
+        return self.info_docente()
+
+class Carrera(models.Model):
+    nombre = models.CharField(max_length=60)
+    facultad = models.CharField(max_length=200)
+    inicio_periodo = models.DateField()
+    final_periodo = models.DateField()
+
+    def info_carrera(self):
+        return "|{}|{}| periodo: {} hasta {}".format(self.nombre, self.facultad, self.inicio_periodo, self.final_periodo)
+    def __str__(self):
+        return self.info_carrera()
+
+class Asignatura(models.Model):
+    carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE, default=0)
+    nombre = models.CharField(max_length=60)
+    ciclo = models.CharField(max_length=20)
+    paralelo = models.CharField(max_length=5)
+
+    def info_asignatura(self):
+        return "{} - {} - {} - {}".format(self.carrera, self.nombre, self.ciclo, self.paralelo)
+
+    def __str__(self):
+        return self.info_asignatura()
