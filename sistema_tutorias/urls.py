216347a52fdc63c82a9db55820591b7204b4df3e
vladimirCeli <vladimir.celi@unl.edu.ec>
2023-07-19 15:31:11 -0500

    Revert "Create Login, Logout, Register Views"
    This reverts commit f00243b03cba4e592329c80b5157ffd9830a2340.

--- sistema_tutorias/urls.py
+++ sistema_tutorias/urls.py
@@ -3,7 +3,4 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout')
 ]
