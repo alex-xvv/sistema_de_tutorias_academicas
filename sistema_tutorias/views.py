f00243b03cba4e592329c80b5157ffd9830a2340
vladimirCeli <vladimir.celi@unl.edu.ec>
2023-07-18 23:12:15 -0500

    Create Login, Logout, Register Views

--- sistema_tutorias/views.py
+++ sistema_tutorias/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import authenticate as auth_authenticate
+from django.db import IntegrityError
 
 # Vistas de las diferentes paginas del proyecto.
 def index(request):
@@ -7,3 +12,31 @@ def index(request):
 
 def about(request):
     return render(request, 'html/about.html')
+
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'html/registro.html', {'form': UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                auth_login(request, user)
+                return redirect('/')
+            except IntegrityError:
+                return render(request, 'html/registro.html', {'form': UserCreationForm(), 'error': 'El nombre de usuario ya existe. Por favor, elija otro nombre.'})
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'html/iniciosesion.html', {'form': AuthenticationForm()})
+    else:
+        user = auth_authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'html/iniciosesion.html', {'form': AuthenticationForm(), 'error': 'Usuario y/o contraseña incorrectos.'})
+        else:
+            auth_login(request, user)
+            return redirect('/')
+
+def logout(request):
+    auth_logout(request)
+    return redirect('/')
